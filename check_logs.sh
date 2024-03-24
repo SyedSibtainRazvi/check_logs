a4a9f2275d99dd36064c320b8a80e139b0cf39d2
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-03-25 03:30:40 +0530

    update yml file

--- check_logs.sh
+++ check_logs.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
-GIT_EXECUTABLE=$(which git)
+GIT_EXECUTABLE=""
+for dir in $(echo "$PATH" | tr ':' '\n'); do
+    if [ -x "$dir/git" ]; then
+        GIT_EXECUTABLE="$dir/git"
+        break
+    fi
+done
+
+if [ -z "$GIT_EXECUTABLE" ]; then
+    echo "ERROR: Git executable not found. Please make sure git is installed and accessible in your PATH."
+    exit 1
+fi
 
 if "$GIT_EXECUTABLE" diff --name-only "$GITHUB_BASE_REF" "$GITHUB_HEAD_REF" | xargs grep -En '\bconsole\.log\b'; then
   echo "ERROR: Console.log statements found. Please remove them before merging the pull request."
