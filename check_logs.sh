d65c1044cfb12b6d63d1866cd1b987e01f9bec2a
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-03-25 03:18:32 +0530

    update yml file

--- check_logs.sh
+++ check_logs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Fetch the list of changed files in the pull request
 changed_files=$(git show --name-only "$GITHUB_SHA")
 
-# Check for console.log statements in changed files
 if echo "$changed_files" | grep -E '\.js$' | xargs grep -En '\bconsole\.log\b'; then
   echo "ERROR: Console.log statements found. Please remove them before merging the pull request."
   exit 1
